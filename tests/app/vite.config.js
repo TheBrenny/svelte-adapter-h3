18d07bf0e33805b58bff78aaafe5569c39e4cc1d
ROBOT <tkata12@outlook.jp>
2025-07-20 14:47:44 +0900

    fix: update package.json to include exports and files fields; clean up vite.config.js

--- tests/app/vite.config.js
+++ tests/app/vite.config.js
@@ -2,10 +2,5 @@ import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 
 export default {
-  plugins: [enhancedImages(), sveltekit()],
-  server: {
-    fs: {
-      allow: ["../../packages/kit"]
-    }
-  }
+  plugins: [enhancedImages(), sveltekit()]
 };
